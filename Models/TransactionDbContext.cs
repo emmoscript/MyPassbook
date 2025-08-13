0aafd522ac7246c4294571d90992e92f01102bfd
emmoscript <emilmr0309@gmail.com>
2025-08-12 23:28:52 -0400

    first commit

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyPassbook.Models
+{
+    public class TransactionDbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
